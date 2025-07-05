7d0e1671fc1969ca7fbcc349f72f9e81379a5a7a
francis-collab <f.mutabazi1@alustudent.com>
2025-07-05 22:21:32 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="bg-blue-900 text-white p-5 text-center">
       <h1 className="text-4xl font-bold">Stroke Awareness Hub</h1>
       <nav className="mt-3 space-x-4">
-        <a href="/" className="hover:underline">Home</a>
-        <a href="/symptoms" className="hover:underline">Symptoms</a>
-        <a href="/prevention" className="hover:underline">Prevention</a>
-        <a href="/treatment" className="hover:underline">Treatment</a>
+        <Link to="/" className="hover:underline">Home</Link>
+        <Link to="/symptoms" className="hover:underline">Symptoms</Link>
+        <Link to="/prevention" className="hover:underline">Prevention</Link>
+        <Link to="/treatment" className="hover:underline">Treatment</Link>
       </nav>
       <hr className="mt-4 border-white" />
     </header>
